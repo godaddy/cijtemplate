afd4a278a550c8d0e1cb3f69059c961adc3016ef
Dave Koopman <dkoopman@godaddy.com>
2015-05-08 10:45:12 -0700

    fixed

--- cijtemplate/views.py
+++ cijtemplate/views.py
@@ -3,9 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
-
-def aBad_FUnctionname(request):
-    asdf = 123
-    asdf = 345
-    sdfffdfs = 345435
-    return None
