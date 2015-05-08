e841920a61b6ce495124bdca7f67a1af705ac5e6
Dave Koopman <dkoopman@godaddy.com>
2015-05-07 18:27:05 -0700

    Remove bad method

--- cijtemplate/views.py
+++ cijtemplate/views.py
@@ -3,9 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
-
-def foobar_Blah(request):
-    something = 1
-    anotherThing_e = 2
-    andAnother = 4
-    return render(request, 'home.html', {})
