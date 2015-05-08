114e40bdaba917c074707444c7c9152e5b881198
Dave Koopman <dkoopman@godaddy.com>
2015-05-07 18:24:11 -0700

    Bad view method added to break, an example

--- cijtemplate/views.py
+++ cijtemplate/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def foobar_Blah(request):
+    something = 1
+    anotherThing_e = 2
+    andAnother = 4
+    return render(request, 'home.html', {})
