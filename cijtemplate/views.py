2c4c263aea0c6a38835855ab30a49447674620eb
Dave Koopman <dkoopman@godaddy.com>
2015-05-09 14:53:49 -0700

    fixed it

--- cijtemplate/views.py
+++ cijtemplate/views.py
@@ -3,10 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
-
-def aReally_BadMethodNameThat_iswaytoolongforthisthing(request):
-    asdf = 123
-    dfg = 345
-    dfg =567
-    poi = 345
-    return None
