6281ebca1cc960aadf48c71cb75ca8be7e3073c9
Dave Koopman <davek@godaddy.com>
2015-12-07 16:42:51 -0700

    self signed cert on nginx, enable ssl only

--- puppet/modules/hosttype_jenkins/manifests/init.pp
+++ puppet/modules/hosttype_jenkins/manifests/init.pp
@@ -9,19 +9,35 @@ class hosttype_jenkins {
       host          => '127.0.0.1',
       context_path  => '/sonar',
     }
-    class { 'nginx': }
+    $certdir = "/etc/pki/tls/certs"
+    exec {'create_self_signed_sslcert_for_nginx':
+      command => "openssl req -newkey rsa:2048 -nodes -keyout server.key  -x509 -days 365 -out server.crt -subj '/CN=${::fqdn}'",
+      cwd     => $certdir,
+      creates => [ "${certdir}/server.key", "${certdir}/server.crt", ],
+      path    => ["/usr/bin", "/usr/sbin"],
+      unless => "/usr/bin/test -f ${certdir}/server.key && /usr/bin/test -f ${certdir}/server.crt",
+    }
+    class { 'nginx': 
+        require => Exec['create_self_signed_sslcert_for_nginx'],
+    }
     nginx::resource::upstream { 'sonar':
         members => [
             '127.0.0.1:9000',
         ],
     }
-    nginx::resource::vhost { 'default':
-       www_root => '/var/www/html', 
-    }
-    nginx::resource::location { 'default':
+    nginx::resource::location { 'sonar':
         location => '/sonar',
         proxy => 'http://sonar',
-        vhost => 'default'
+        vhost => 'myserver',
+        ssl => true,
+        ssl_only => true,
+    }
+    nginx::resource::vhost { 'myserver':
+        www_root => '/var/www/html', 
+        ssl => true,
+        listen_port => 443,
+        ssl_port => 443,
+        ssl_cert             => "${certdir}/server.crt",
+        ssl_key              => "${certdir}/server.key",
     }
 }
-
